6a238b760afdd4499b6755bff5ecad9e61c10b5e
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2022-08-31 10:34:03 +0200

    refactor: dir naming

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
         'Programming Language :: Python :: 3 :: Only'
     ],
     keywords='ediel energy parser consumption svk sunlabs edifact',
-    packages=['ediel-parser'],
+    packages=['ediel_parser'],
     install_requires=[]
 )
