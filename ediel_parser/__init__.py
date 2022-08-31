38794550f729a0b76867477ad66f7dac7215888f
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2022-08-31 10:26:38 +0200

    fix: relative imports

--- ediel_parser/__init__.py
+++ ediel_parser/__init__.py
@@ -1,3 +1,3 @@
-from ediel_parser.lib import EDIParser, EDICommunicator, Segment, UNSegment, segmentDefinitions, ediTools
+from .lib import EDIParser, EDICommunicator, Segment, UNSegment, segmentDefinitions, ediTools
 
 __all__ = [EDIParser, EDICommunicator, Segment, UNSegment, segmentDefinitions, ediTools]
