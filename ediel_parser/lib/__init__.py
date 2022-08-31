36aa89f183a946137ea11da847a75706b24ede4b
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2022-08-31 10:28:43 +0200

    fix: set all lib modules to __all__

---
+++ ediel_parser/lib/__init__.py
@@ -0,0 +1,3 @@
+from ediel_parser.lib import EDIParser, EDICommunicator, Segment, UNSegment, segmentDefinitions, ediTools
+
+__all__ = [EDIParser, EDICommunicator, Segment, UNSegment, segmentDefinitions, ediTools]
